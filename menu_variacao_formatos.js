82cca903c250350dde2b3d0e7aaafbbfb849786b
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-10-14 18:51:32 -0300

    Add files via upload

---
+++ menu_variacao_formatos.js
@@ -0,0 +1,72 @@
+const facebookSwitchBts = document.querySelectorAll('#formatos_facebook menu li')
+
+const FBImagemSpecs = document.querySelector('#facebook_imagem_specs')
+const FBVideoSpecs = document.querySelector('#facebook_video_specs')
+const FBCarosselSpecs = document.querySelector('#facebook_carrossel_specs')
+
+let showingFBImageSpecs = true;
+let showingFBVideoSpecs = false;
+let showingFBCarrosselSpecs = false;
+
+function checkFBSwitch(e) {
+    
+    if(e.target.classList.contains('facebook_imagem_bt')) {
+        showingFBImageSpecs = true;
+        showingFBVideoSpecs = false;
+        showingFBCarrosselSpecs = false;
+
+        FBVideoSpecs.classList.remove('switched')
+        FBCarosselSpecs.classList.remove('switched')
+
+        setTimeout(() => {
+            FBImagemSpecs.classList.add('switched')
+        }, 100);
+
+        for (i=0;i<facebookSwitchBts.length;i++) {
+            facebookSwitchBts[i].classList.remove('active_type')
+        }
+
+            e.target.classList.add('active_type')
+
+    } else if(e.target.classList.contains('facebook_video_bt')) {
+        showingFBImageSpecs = false;
+        showingFBVideoSpecs = true;
+        showingFBCarrosselSpecs = false;
+
+        FBImagemSpecs.classList.remove('switched')
+        FBCarosselSpecs.classList.remove('switched')
+
+        setTimeout(() => {
+            FBVideoSpecs.classList.add('switched')
+        }, 100);
+
+        for (i=0;i<facebookSwitchBts.length;i++) {
+            facebookSwitchBts[i].classList.remove('active_type')
+        }
+        
+        e.target.classList.add('active_type')
+
+    } else if(e.target.classList.contains('facebook_carrossel_bt')) {
+        showingFBImageSpecs = false;
+        showingFBVideoSpecs = false;
+        showingFBCarrosselSpecs = true;
+
+        FBImagemSpecs.classList.remove('switched')
+        FBVideoSpecs.classList.remove('switched')
+
+        setTimeout(() => {
+            FBCarosselSpecs.classList.add('switched')
+        }, 100);
+
+        for (i=0;i<facebookSwitchBts.length;i++) {
+            facebookSwitchBts[i].classList.remove('active_type')
+        }
+        
+        e.target.classList.add('active_type')
+        
+    }
+}
+
+for (i=0;i<facebookSwitchBts.length;i++) {
+    facebookSwitchBts[i].addEventListener('click', checkFBSwitch)
+}
