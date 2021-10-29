44541bdbf50da5128e7476a26851703cb79bfb83
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-10-29 13:32:36 -0300

    Add files via upload

---
+++ submenus.js
@@ -0,0 +1,259 @@
+{/* <section id="interscroller_video" class="formato_sec interscroller">
+                    <article>
+                        <h3>Formatos diferenciados</h3>
+                        <h2>Interscroller Video</h2>
+
+                        <menu>
+                            <li class="active_type" id="Interscroller_RM_bt">Video</li>
+                            <li class="" id="Interscroller_video_bt">Video vertical</li>
+                            <li class="" id="Interscroller_display_bt">Video + BG</li>
+                        </menu>
+        
+                        <p>Consectetur ab, aut distinctio dolores commodi suscipit vel tenetur voluptatem quod repellat dignissimos repudiandae! Quis neque nihil ut dolorem aspernatur tenetur veniam.</p>
+
+                            <ul class="bullets" id="Interscroller_RM_bt">
+                                <li class="highlight_bullet">
+                                    <div class="bullet_icon01" title="Tipo de arquivo">
+                                        <img src="assets/icons/smartphone.svg" alt="">
+                                    </div>
+                                    <p>Mobile</p>
+                                </li>
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de arquivo">
+                                        <img src="assets/icons/file_type.svg" alt="">
+                                    </div>
+                                    <p>Peça em HTML5 (.zip)</p>
+                                </li>
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de comercialização">
+                                        <img src="assets/icons/money.svg" alt="">
+                                    </div>
+                                    <p>Comercializado por CPM</p>
+                                </li>
+    
+                                <div>
+                                    <li>
+                                        <div class="bullet_icon01" title="Tipo de comercialização">
+                                            <img src="assets/icons/design.svg" alt="">
+                                        </div>
+                                            <p>Especificações de design</p>
+                                    </li>
+        
+                                    <li>
+                                        <span class="text_bullet">
+                                            <ul>
+                                                <li><p>Dimensões: 320x480px</p></li>
+                                            </ul>
+                                        </span>
+                                    </li>
+                                </div>
+    
+                                <div>
+                                    <li>
+                                        <div class="bullet_icon01" title="Tipo de comercialização">
+                                            <img src="assets/icons/engine.svg" alt="">
+                                        </div>
+                                            <p>Requisitos técnicos</p>
+                                    </li>
+    
+                                    <li>
+                                        <span class="text_bullet">
+                                            <ul>
+                                                <li><p>Fornecer URL de destino</p></li>
+                                                <li><p>Peso máximo: 380kb</p></li>
+                                            </ul>
+                                        </span>
+                                    </li>
+                                </div>
+    
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de comercialização">
+                                        <img src="assets/icons/info.svg" alt="">
+                                    </div>
+                                    <p>
+                                        <span class="specs_bt">Prazos e boas práticas</span>
+                                    </p>
+                                </li>
+                            </ul>
+
+                            <ul class="bullets hidden_spec" id="Interscroller_video_bt">
+                                <li class="highlight_bullet">
+                                    <div class="bullet_icon01" title="Tipo de arquivo">
+                                        <img src="assets/icons/smartphone.svg" alt="">
+                                    </div>
+                                    <p>Desktop</p>
+                                </li>
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de arquivo">
+                                        <img src="assets/icons/file_type.svg" alt="">
+                                    </div>
+                                    <p>Peça em HTML5 (.zip)</p>
+                                </li>
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de comercialização">
+                                        <img src="assets/icons/money.svg" alt="">
+                                    </div>
+                                    <p>Comercializado por CPM</p>
+                                </li>
+    
+                                <div>
+                                    <li>
+                                        <div class="bullet_icon01" title="Tipo de comercialização">
+                                            <img src="assets/icons/design.svg" alt="">
+                                        </div>
+                                            <p>Especificações de design</p>
+                                    </li>
+        
+                                    <li>
+                                        <span class="text_bullet">
+                                            <ul>
+                                                <li><p>Dimensões: 320x480px</p></li>
+                                            </ul>
+                                        </span>
+                                    </li>
+                                </div>
+    
+                                <div>
+                                    <li>
+                                        <div class="bullet_icon01" title="Tipo de comercialização">
+                                            <img src="assets/icons/engine.svg" alt="">
+                                        </div>
+                                            <p>Requisitos técnicos</p>
+                                    </li>
+    
+                                    <li>
+                                        <span class="text_bullet">
+                                            <ul>
+                                                <li><p>Fornecer URL de destino</p></li>
+                                                <li><p>Peso máximo: 380kb</p></li>
+                                            </ul>
+                                        </span>
+                                    </li>
+                                </div>
+    
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de comercialização">
+                                        <img src="assets/icons/info.svg" alt="">
+                                    </div>
+                                    <p>
+                                        <span class="specs_bt">Prazos e boas práticas</span>
+                                    </p>
+                                </li>
+                            </ul>
+
+                            <ul class="bullets hidden_spec" id="Interscroller_display_bt">
+                                <li class="highlight_bullet">
+                                    <div class="bullet_icon01" title="Tipo de arquivo">
+                                        <img src="assets/icons/smartphone.svg" alt="">
+                                    </div>
+                                    <p>Mobile / Desktop</p>
+                                </li>
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de arquivo">
+                                        <img src="assets/icons/file_type.svg" alt="">
+                                    </div>
+                                    <p>Peça em HTML5 (.zip)</p>
+                                </li>
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de comercialização">
+                                        <img src="assets/icons/money.svg" alt="">
+                                    </div>
+                                    <p>Comercializado por CPM</p>
+                                </li>
+    
+                                <div>
+                                    <li>
+                                        <div class="bullet_icon01" title="Tipo de comercialização">
+                                            <img src="assets/icons/design.svg" alt="">
+                                        </div>
+                                            <p>Especificações de design</p>
+                                    </li>
+        
+                                    <li>
+                                        <span class="text_bullet">
+                                            <ul>
+                                                <li><p>Dimensões: 320x480px</p></li>
+                                            </ul>
+                                        </span>
+                                    </li>
+                                </div>
+    
+                                <div>
+                                    <li>
+                                        <div class="bullet_icon01" title="Tipo de comercialização">
+                                            <img src="assets/icons/engine.svg" alt="">
+                                        </div>
+                                            <p>Requisitos técnicos</p>
+                                    </li>
+    
+                                    <li>
+                                        <span class="text_bullet">
+                                            <ul>
+                                                <li><p>Fornecer URL de destino</p></li>
+                                                <li><p>Peso máximo: 380kb</p></li>
+                                            </ul>
+                                        </span>
+                                    </li>
+                                </div>
+    
+                                <li>
+                                    <div class="bullet_icon01" title="Tipo de comercialização">
+                                        <img src="assets/icons/info.svg" alt="">
+                                    </div>
+                                    <p>
+                                        <span class="specs_bt">Prazos e boas práticas</span>
+                                    </p>
+                                </li>
+                            </ul>
+
+                    </article>
+
+                    <div class="img_ctnr smartphone_ctnr" id="Interscroller_RM_bt">
+                        <img src="assets/smartphone.png" alt="Formatos de anúncio Search" draggable="false">
+                        <a href="#" target="_blank" class="live_demo">Live Demo</a>
+                    </div>
+                    <div class="img_ctnr smartphone_ctnr hidden_spec" id="Interscroller_video_bt">
+                        <img src="assets/smartphone.png" alt="Formatos de anúncio Search" draggable="false">
+                        <a href="#" target="_blank" class="live_demo">Live Demo</a>
+                    </div>
+                    <div class="img_ctnr hidden_spec" id="Interscroller_display_bt">
+                        <video class="desktop_video" src="assets/dekstop_video.mp4" autoplay muted loop></video>
+                        <video class="mobile_video" src="assets/mobile_video.mp4" autoplay muted loop></video>                        
+                        <img src="assets/mobile_desktop.png" alt="Formatos rich media exclusivos e interativos" draggable="false">
+                        <a href="#" target="_blank" class="live_demo">Live Demo</a>
+                    </div>
+                </section>
+
+<!-- SCRIPT DE SUBMENU DE MANUAL DOS FORMATOS --> */}
+
+const handleClick = function (event) {  
+
+  const itemsMenu = Array.from(event.target.parentNode.getElementsByTagName("li"))
+
+  itemsMenu.map(item => item.classList.remove("active_type"))
+
+  event.target.classList.add("active_type")
+
+  const itemsAsides = Array.from(event.target.parentNode.parentNode.getElementsByTagName("ul"))
+
+  itemsAsides.map(aside => aside.classList.add("hidden_spec"))
+
+  document.querySelector("ul#" + event.target.id).classList.remove("hidden_spec");
+
+  var previews = Array.from(event.target.parentNode.parentNode.parentNode.getElementsByClassName("img_ctnr"));
+
+//   previews.map(preview => preview.classList.add("hidden_spec"));
+
+//   document.querySelector("div#" + event.target.id).classList.remove("hidden_spec");
+
+//   document.querySelector("div#" + event.target.id).style.opacity = 1;
+
+}
+
+var sections = Array.from(document.getElementsByTagName("main")[0].getElementsByTagName("section"));
+
+var menus = sections.filter(section => section.getElementsByTagName("menu")[0]).map(section => section.getElementsByTagName("menu")[0]);
+
+var items = menus.reduce((acc, menu) => [...acc, ...menu.getElementsByTagName("li")], []);
+
+items.map(item => item.onclick = handleClick);
